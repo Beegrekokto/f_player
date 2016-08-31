50c40a908e91936c80b861c88286311a44d86624
Aashik <beegrekokto@gmail.com>
2016-08-31 12:09:19 +0545

    editing_method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,6 +9,11 @@ class PlayersController < ApplicationController
 		redirect_to root_path
 	end
 
+ def edit
+ 	@player = Player.find(params[:id])
+ 	
+ end
+
 private
 	   def player_params
 	   	params.require(:player).permit(:name, :age, :nationality, :wiki)
