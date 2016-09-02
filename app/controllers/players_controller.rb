a7961ba9d45eb0e9f8d0e2bcd8aaa2326a6c53e4
Take Mouri <takehiromouri@gmail.com>
2016-09-02 14:33:02 +0545

    change design

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,52 +1,39 @@
 class PlayersController < ApplicationController
 
- def index
-		@players = Player.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
- end
+	def index
+		@players = Player.order("created_at ASC").paginate(:page => params[:page], :per_page => 3)
+		@all_player_ids = Player.all.pluck(:id)
+	end
 
 	def create
 		@player = Player.create(player_params)
 		redirect_to root_path
 	end
 
- def edit
- 	@player = Player.find(params[:id])
+	def edit
+		@player = Player.find(params[:id])
+	end
+
  	def update
- 	 @player = Player.find(params[:id])
- 	 if @player.update(player_params)
- 		redirect_to root_path
- 	 else
- 		redirect_to edit_player_path(params[:id])
- 	 end
+		@player = Player.find(params[:id])
+		if @player.update(player_params)
+			redirect_to root_path
+		else
+			redirect_to edit_player_path(params[:id])
+		end
  	end
- end
- 	
- 
-  
-  
 
- 
+	def destroy
+		@player = Player.find(params[:id])
+		@player.destroy
+		redirect_to root_path
+	end
 
- def destroy
- 	@player = Player.find(params[:id])
- 	@player.destroy
- 	redirect_to root_path
- 	
- end
- 	
- 
+	private
 
-private
-	   def player_params
-	   	params.require(:player).permit(:name, :age, :nationality, :wiki)
-	   end
-
-
-
-
-		
-	  
-		
+	def player_params
+		params.require(:player).permit(:name, :age, :nationality, :wiki)
+	end
 	
 end
 
